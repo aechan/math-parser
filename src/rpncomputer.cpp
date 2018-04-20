78ca0409548d7bfd81a97a33006ec334bb381bad
Alec Chan <me@alecchan.io>
2018-04-20 17:45:26 -0500

    rpncomputer is fully working now

--- src/rpncomputer.cpp
+++ src/rpncomputer.cpp
@@ -8,13 +8,21 @@ using namespace std;
 
 int rpncomputer::evaluate(deque<Token> infix) {
     stack<Token> st;
+
+    // for each token in the RPN expression
     for(auto token: infix) {
+        // if the token is a number push it to top of stack
         if(token.type == Token::Type::Number) {
             st.push(token);
+        // if the token is an operator
         } else if(token.type == Token::Type::Operator) {
+            // get the top two values and pop them off the stack
             int a = st.top().value;
             st.pop();
             int b = st.top().value;
+            st.pop();
+
+            // stores the result of this operation
             int res;
             if(token.str == "^") {
                 res = a^b;
@@ -31,6 +39,7 @@ int rpncomputer::evaluate(deque<Token> infix) {
             else if (token.str == "-") {
                 res = a - b;
             }
+            // push the result back onto the stack
             st.push(Token(Token::Type::Number,res, to_string(res)));
         }
 
